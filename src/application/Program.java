2ca2e400bdb7764bfcce7e00592e985096b2a2c5
alissonfgc <alissonfgc01@gmail.com>
2024-07-02 03:55:39 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,10 +7,13 @@ import model.entities.Seller;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
@@ -33,17 +36,21 @@ public class Program {
             System.out.println(s);
         }
 
-        System.out.println("\n=========INSERT===========");
-        Seller newSeller = new Seller(null, "Chris tiro certo 4", "chris@gmail.com", LocalDate.now(), 4200.00, new Department(1, null));
-        sellerDao.insert(newSeller);
-        System.out.println(newSeller);
+//        System.out.println("\n=========INSERT===========");
+//        Seller newSeller = new Seller(null, "Chris tiro certo 4", "chris@gmail.com", LocalDate.now(), 4200.00, new Department(1, null));
+//        sellerDao.insert(newSeller);
+//        System.out.println(newSeller);
+//
+//        System.out.println("\n=========UPDATE===========");
+//        newSeller = sellerDao.findById(10);
+//        newSeller.setName("Bruce Waine da Bhaia");
+//        sellerDao.update(newSeller);
+//        System.out.println(newSeller);
 
-        System.out.println("\n=========UPDATE===========");
-        newSeller = sellerDao.findById(10);
-        newSeller.setName("Bruce Waine da Bhaia");
-        sellerDao.update(newSeller);
-        System.out.println(newSeller);
-
+        System.out.println("\n=========DELETE===========");
+        System.out.print("Enter the id for delete: ");
+        Integer id = sc.nextInt();
+        sellerDao.deleteById(id);
 
     }
 }
